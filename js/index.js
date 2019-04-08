c53a2601629396e2484e24f8b21f7a4e8d98d9d3
unknown <bejolo018@gmail.com>
2019-04-08 15:21:43 -0500

    added selectors in js

--- js/index.js
+++ js/index.js
@@ -40,3 +40,9 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+let cta = document.getElementById("cta")
+cta.setAttribute()
+
+let middleimg = document.getElementsById("middleimg");
+middleimg.setAttribute()
